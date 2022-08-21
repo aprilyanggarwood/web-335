d3d35dd50c9b92ec36a8fc3c6a2a192d02d68021
aprilyanggarwood <yangyyj822@hotmail.com>
2022-08-21 01:36:05 -0700

    created a python file

---
+++ week_2/yang_myworld.py
@@ -0,0 +1,11 @@
+
+# Title: yang_myworld.py
+# Author: Professor Krasso
+# Date: 08 17 2022
+# Modified By: April Yang
+# Description: Introduction to python
+
+
+#Prints to terminal "You are now in yang's world!"
+print("You are now in yang's world!")
+
